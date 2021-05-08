8a23d671a69b3baee19d9c2b9b0be0901ffedb83
Can Altıparmak <can6parmak@gmail.com>
2021-05-08 20:20:27 +0300

    Fix SRI and CSP issues

---
+++ assets/mathjax.js
@@ -0,0 +1,17 @@
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$','$$'], ['\\[', '\\]']],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code'],
+    renderActions: { addMenu: [] }
+  }
+};
+
+window.onload = (event) => {
+  document.querySelectorAll("mjx-container").forEach(function(x){
+    x.parentElement.classList += 'has-jax'})
+};
