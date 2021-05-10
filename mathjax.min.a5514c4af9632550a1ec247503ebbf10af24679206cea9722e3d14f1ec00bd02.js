787f970f7d6751be9a911563d00176c2e37e4049
c6p <c6p@users.noreply.github.com>
2021-05-10 06:54:51 +0000

    deploy: 5b258a4b05ed3588665cabd3315294fdb25677f3

--- mathjax.min.a5514c4af9632550a1ec247503ebbf10af24679206cea9722e3d14f1ec00bd02.js
+++
@@ -1,1 +0,0 @@
-window.MathJax={tex:{inlineMath:[['$','$'],['\\(','\\)']],displayMath:[['$$','$$'],['\\[','\\]']],processEscapes:true,processEnvironments:true},options:{skipHtmlTags:['script','noscript','style','textarea','pre','code'],renderActions:{addMenu:[]}}};
