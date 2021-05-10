b2d5950e3a8d65188019b4c8d766adc3e77d4259
Can Altıparmak <can6parmak@gmail.com>
2021-05-10 13:34:18 +0300

    Finalize CSP settings

--- helpers/sri.ps1
+++ helpers/sri.ps1
@@ -1,3 +1,3 @@
 hugo --minify
 (rg -oIN '<script.*?(sha\d{3}-.{43}=)\"' -r '''$1''' public | sort -unique) -join ' ' | out-file -encoding ASCII -noNewline data/hash/script.txt 
-(rg -oIN '<link.*?(sha\d{3}-.{43}=)\"' -r '''$1''' public | sort -unique) -join ' ' | out-file -encoding ASCII -noNewline data/hash/style.txt
+# (rg -oIN '<link.*?(sha\d{3}-.{43}=)\"' -r '''$1''' public | sort -unique) -join ' ' | out-file -encoding ASCII -noNewline data/hash/style.txt
